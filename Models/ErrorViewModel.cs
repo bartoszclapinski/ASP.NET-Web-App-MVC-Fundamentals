50029e256526da2577d563bb2ce2a916d581d449
Bartosz Clapinski <bartosz.clapinski@gmail.com>
2022-12-17 21:50:04 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_Web_App_MVC_Fundamentals.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
